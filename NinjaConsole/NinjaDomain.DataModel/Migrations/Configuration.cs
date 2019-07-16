56b8ba1be1f5a41155993984c878792e1d92e0f4
s00083446 <s00083446@mail.itsligo.ie>
2019-07-16 23:54:44 +0100

    Finished data migration

--- NinjaConsole/NinjaDomain.DataModel/Migrations/Configuration.cs
+++ NinjaConsole/NinjaDomain.DataModel/Migrations/Configuration.cs
@@ -1,5 +1,6 @@
 namespace NinjaDomain.DataModel.Migrations
 {
+    using NinjaDomain.Classes;
     using System;
     using System.Collections.Generic;
     using System.Data.Entity.Migrations;
@@ -10,26 +11,56 @@ namespace NinjaDomain.DataModel.Migrations
             AutomaticMigrationsEnabled = false;
         }
 
-        protected override void Seed(NinjaDomain.DataModel.NinjaContext context)
+        protected override void Seed(NinjaContext context)
         {
-            context.Clans.AddOrUpdate(new Classes.Clan[]
+            context.Clans.AddOrUpdate(new Clan[]
                 {
-                new Classes.Clan {Id = 1, ClanName = "Giles Clan", Ninjas = new List<Classes.Ninja>()
+                new Clan {Id = 1, ClanName = "Giles Clan", Ninjas = new List<Classes.Ninja>()
                 {
                 // We can add Ninjas to the Clan as we create it
                     new Classes.Ninja
                     {
                         Id = 1,  DateOfBirth = DateTime.Parse("12/02/2006"),
-                        EquipmentOwned = new List<Classes.NinjaEquipment>()
+                        EquipmentOwned = new List<NinjaEquipment>()
                         {
-                            new Classes.NinjaEquipment {Name = "Sweet thing", Id = 1, Type = Classes.EquipmentType.Weapon }
+                            new NinjaEquipment {Name = "Sweet thing", Id = 1, Type = EquipmentType.Weapon }
                         },
                         Name = "Ninja Giles", ServedInOniwaban = true
                 },
+                    new Classes.Ninja
+                    {
+                        Id = 1,  DateOfBirth = DateTime.Parse("02/02/2000"),
+                        EquipmentOwned = new List<NinjaEquipment>()
+                        {
+                            new NinjaEquipment {Name = "Comona XS", Id = 2, Type = EquipmentType.Tool}
+                        },
+                        Name = "Ninja Fred", ServedInOniwaban = false
+                }
                     // Next Clan record goes here
              }
-             }
-         });
+             },
+                 new Clan {Id = 2, ClanName = "Ninja Girls Clan", Ninjas = new List<Classes.Ninja>()
+                {
+                // We can add Ninjas to the Clan as we create it
+                    new Classes.Ninja
+                    {
+                        Id = 1,  DateOfBirth = DateTime.Parse("12/02/1900"),
+                        EquipmentOwned = new List<NinjaEquipment>()
+                        {
+                            new NinjaEquipment {Name = "Sour Grapes", Id = 3, Type = EquipmentType.Weapon }
+                        },
+                        Name = "Ninja Martha", ServedInOniwaban = true
+                },
+                     new Classes.Ninja
+                    {
+                        Id = 1,  DateOfBirth = DateTime.Parse("10/02/1990"),
+                        EquipmentOwned = new List<NinjaEquipment>()
+                        {
+                            new NinjaEquipment {Name = "Fishing Net", Id = 4, Type = EquipmentType.Outwear }
+                        },
+                        Name = "Ninja Mary", ServedInOniwaban = true
+                }
+         } } });
         }
     }
 }
--- NinjaConsole/NinjaDomain.DataModel/NinjaContext.cs
+++ NinjaConsole/NinjaDomain.DataModel/NinjaContext.cs
@@ -11,5 +11,8 @@ namespace NinjaDomain.DataModel
 {
     public class NinjaContext:DbContext
     {
+        public DbSet<Ninja> Ninjas { get; set; }
+        public DbSet<Clan> Clans { get; set; }
+        public DbSet<NinjaEquipment> Equipment { get; set; }
     }
 }
